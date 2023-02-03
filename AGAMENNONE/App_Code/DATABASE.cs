eb12ca3cfc9692688d19dbf606d632c3332661aa
Claudio Scuderi <claudioscuderi.gd@gmail.com>
2023-02-03 11:29:41 +0100

    Progetto IN

---
+++ AGAMENNONE/App_Code/DATABASE.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+
+public class DATABASE
+{
+    public SqlConnection conn = new SqlConnection();
+    public string query;
+    public SqlCommand cmd = new SqlCommand();
+    public SqlDataAdapter DA = new SqlDataAdapter();
+    public DataTable DT = new DataTable();
+
+    public DATABASE()
+    {
+        conn.ConnectionString = @"Data Source=CLAUDIO_PC\SQLEXPRESS;Initial Catalog=AUTOSALONI;Integrated Security=True;";
+        cmd.Connection = conn;
+    }
+
+    //carica i dati db con una query diretta scritta in SQL
+    public DataTable EseguiQueryRead()
+    {
+        cmd.CommandText = query;
+        cmd.CommandType = CommandType.Text;
+        DataTable DT = new DataTable();
+        DA.SelectCommand = cmd;
+        DA.Fill(DT);
+        return DT;
+    }
+
+    //esegue ins, upd, del sul db
+    public void EseguiQueryNonRead()
+    {
+        cmd.CommandText = query;
+        cmd.CommandType = CommandType.Text;
+        conn.Open();
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    //eseguo una stored procedure senza ritorno di dati
+    public void EseguiSPNonRead()
+    {
+        cmd.CommandText = query;
+        cmd.CommandType = CommandType.StoredProcedure;
+        conn.Open();
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    public DataTable EseguiSPRead()
+    {
+        cmd.CommandText = query;
+        cmd.CommandType = CommandType.StoredProcedure;
+        DA.SelectCommand = cmd;
+        DA.Fill(DT);
+        return DT;
+    }
+}
---
+++ AGAMENNONE/App_Code/UTENTI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+public class UTENTI
+{
+    public int key;
+    public string email;
+    public string password;
+    public string tipo;
+
+    public UTENTI()
+    {
+
+    }
+
+    public void Insert()
+    {
+        DATABASE DB = new DATABASE();
+        DB.query = "UTENTI_Registrazione";
+        DB.cmd.Parameters.AddWithValue("email", email);
+        DB.cmd.Parameters.AddWithValue("password", password);
+        DB.EseguiSPNonRead();
+    }
+
+    public void UpdatePassword()
+    {
+        DATABASE DB = new DATABASE();
+        DB.query = "UTENTI_UpdatePasswordTipo";
+        DB.cmd.Parameters.AddWithValue("chiave", key);
+        DB.cmd.Parameters.AddWithValue("password", password);
+        DB.cmd.Parameters.AddWithValue("tipo", tipo);
+        DB.EseguiSPNonRead();
+    }
+
+    public DataTable Esistenza()
+    {
+        DataTable dt = new DataTable();
+        DATABASE DB = new DATABASE();
+        DB.query = "UTENTI_EmailEsistente";
+        DB.cmd.Parameters.AddWithValue("email", email);
+        dt = DB.EseguiSPRead();
+        return dt;
+    }
+
+    public DataTable Login()
+    {
+        DataTable dt = new DataTable();
+        DATABASE DB = new DATABASE();
+        DB.query = "UTENTI_Login";
+        DB.cmd.Parameters.AddWithValue("email", email);
+        DB.cmd.Parameters.AddWithValue("password", password);
+        dt = DB.EseguiSPRead();
+        return dt;
+    }
+}
